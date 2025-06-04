1a5ca1960d534b63f4b4032b59fd73030b189386
spartha1 <leinadspartha@gmail.com>
2025-06-04 16:13:38 -0600

    Update SQL dump generation time in limanprof.sql

--- public/components/limanprof.sql
+++ public/components/limanprof.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-06-2025 a las 23:01:49
+-- Tiempo de generación: 04-06-2025 a las 23:29:36
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
